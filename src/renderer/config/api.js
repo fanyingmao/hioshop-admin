eb7d34c9156e7db27ef3cfd1af3de9455aa5ac9f
fanyingmao <fanyingmao@gmail.com>
2020-11-29 22:50:26 +0800

    测试配置

--- src/renderer/config/api.js
+++ src/renderer/config/api.js
@@ -1,5 +1,6 @@
-const rootUrl = 'http://www.hiolabs.com:8360/admin/';
-// const rootUrl = 'http://127.0.0.1:8360/admin/';
+// const rootUrl = 'http://www.hiolabs.com:8360/admin/';
+const rootUrl = 'http://127.0.0.1:8360/admin/';
+// const rootUrl = 'http://server:8360/admin/';
 
 const api = {
     rootUrl : rootUrl,
